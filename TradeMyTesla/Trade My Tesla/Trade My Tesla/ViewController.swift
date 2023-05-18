4f6d049a153c2a8b2b95053f28144be1c2b850c8
Aaraiz-Pie <maaraiz@pie-int.com>
2023-05-18 23:21:36 +0500

    Complete UI and add Outlets to view controller

--- TradeMyTesla/Trade My Tesla/Trade My Tesla/ViewController.swift
+++ TradeMyTesla/Trade My Tesla/Trade My Tesla/ViewController.swift
@@ -8,10 +8,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBAction func calculateValue(_ sender: Any) {
+    }
+    
+    @IBOutlet weak var valuation: UILabel!
+    @IBOutlet weak var condition: UISegmentedControl!
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var mileage: UILabel!
+    @IBOutlet weak var upgrades: UISegmentedControl!
+    @IBOutlet weak var model: UISegmentedControl!
+    @IBOutlet weak var stackView: UIStackView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+      
     }
 
 
