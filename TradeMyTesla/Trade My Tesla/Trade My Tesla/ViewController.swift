6bbbdb9fc44f82b4c2489488727926bc34657002
Aaraiz-Pie <maaraiz@pie-int.com>
2023-05-31 12:21:49 +0500

    Add functionality using ML Model

--- TradeMyTesla/Trade My Tesla/Trade My Tesla/ViewController.swift
+++ TradeMyTesla/Trade My Tesla/Trade My Tesla/ViewController.swift
@@ -8,9 +8,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBAction func calculateValue(_ sender: Any) {
-    }
-    
+  
+    let cars = Cars()
     @IBOutlet weak var valuation: UILabel!
     @IBOutlet weak var condition: UISegmentedControl!
     @IBOutlet weak var slider: UISlider!
@@ -18,12 +17,28 @@ class ViewController: UIViewController {
     @IBOutlet weak var upgrades: UISegmentedControl!
     @IBOutlet weak var model: UISegmentedControl!
     @IBOutlet weak var stackView: UIStackView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
       
     }
+    
+    @IBAction func calculateValue(_ sender: Any) {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        formatter.maximumFractionDigits = 0
+        let formattedMileage = formatter.string(for: slider.value) ?? "0"
+        mileage.text = "MILEAGE (\(formattedMileage) miles)"
 
+        if let prediction = try? cars.prediction(model: Double(model.selectedSegmentIndex), premium: Double(upgrades.selectedSegmentIndex), mileage: Double(slider.value), condition: Double(condition.selectedSegmentIndex)) {
+            let clampedValuation = max(2000, prediction.price)
+            formatter.numberStyle = .currency
+            valuation.text = formatter.string(for: clampedValuation)
+        } else {
+            valuation.text = "Error"
+        }
+    }
 
 }
 
